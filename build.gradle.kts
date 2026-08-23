faf18965657f94c29dd10a7eebfd1d46a6af333d
Mointanveerm <mointanveer1008@gmail.com>
2026-08-23 12:35:39 +0500

    Downgrade targetSdk from 34 to 29

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.internet.app"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 29
         versionCode = 1
         versionName = "1.0"
     }
